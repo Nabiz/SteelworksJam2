86fadbe532c1fa9106caec0f63857bc0af04cfa8
Kimel-PK <57668948+Kimel-PK@users.noreply.github.com>
2023-10-27 22:31:31 +0200

    Projectiles script

---
+++ Assets/Scripts/Roguelike/Projectile.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed;
+    public float damage;
+    public float lifetime;
+    public bool destroyOnCollide;
+    public float pierceCooldown;
+    public int pierceLifetime;
+    
+    [SerializeField] private float currentPierceLifetime;
+
+    private void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector2.up * (speed * Time.deltaTime));
+        if (currentPierceLifetime > 0)
+        {
+            currentPierceLifetime -= Time.deltaTime;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Entity"))
+        {
+            if (currentPierceLifetime >= 0)
+            {
+                other.gameObject.GetComponent<Entity>().HP -= damage;
+                currentPierceLifetime = pierceCooldown;
+            }
+
+            if (destroyOnCollide)
+                Destroy(gameObject);
+        }
+    }
+}
