a0adc64d5a0ab9d3d85e01027b13417518f45316
Kimel-PK <57668948+Kimel-PK@users.noreply.github.com>
2023-10-28 23:34:19 +0200

    character branch merge fix

--- Assets/Scripts/Animation/SpriteAnimator.cs
+++ Assets/Scripts/Animation/SpriteAnimator.cs
@@ -4,51 +4,23 @@ using UnityEngine;
 
 public class SpriteAnimator : MonoBehaviour
 {
-    private SpriteSegment[] sprites;
-    
-    [field: SerializeField] public float AnimationSpeed { get; private set; } = 1f;
-
-    [SerializeField] List<SpriteSegment> spriteSegments;
-
-    [SerializeField] List<float> spriteSegmentsOffsets;
-
+	private SpriteSegment[] sprites;
 	private void Awake()
 	{
-        sprites = gameObject.GetComponentsInChildren<SpriteSegment>(includeInactive: false); 
-
-        HashSet<float> offsets = new HashSet<float>(spriteSegmentsOffsets);
-        float offset = offsets.Count * 0.1f;
-
-        for (int i = 0; i < spriteSegments.Count; ++i)
-        {
-            spriteSegments[i].Init(spriteSegmentsOffsets[i]);
-        }
-
-        for (int i = 0; i < sprites.Length; ++i) //then make it check if sprite schould react to dir change
-        {
-            sprites[i].ChangeDirection();
-            sprites[i].Init(i * 0.1f + offset);
-        }
+		sprites = gameObject.GetComponentsInChildren<SpriteSegment>(includeInactive: false);
 	}
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    void OnValidate() {
-        for (int i = 0; i < spriteSegmentsOffsets.Count; ++i) //then make it check if sprite schould react to dir change
-        {
-            spriteSegmentsOffsets[i] = Mathf.Clamp01(spriteSegmentsOffsets[i]);
-        }
-    }
-
-    public void SetDirectionAll(Enums.Direction dir)
+	// Update is called once per frame
+	void Update()
 	{
-        for (int i = 0; i < sprites.Length; ++i) //then make it check if sprite schould react to dir change
+        
+	}
+
+	public void SetDirectionAll(Enums.Direction dir)
+	{
+		for (int i = 0; i < sprites.Length; ++i) //then make it check if sprite schould react to dir change
 		{
-            sprites[i].SetDirection(dir);
+			sprites[i].SetDirection(dir);
 		}
 	}
 }
--- Assets/Scripts/Animation/SpriteSegment.cs
+++ Assets/Scripts/Animation/SpriteSegment.cs
@@ -1,57 +1,42 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SpriteSegment : MonoBehaviour
 {
-    [SerializeField] protected Enums.Direction dir;
+	[SerializeField] protected Enums.Direction dir;
 
-    public virtual void ChangeDirection()
+	public virtual void ChangeDirection()
 	{
-        Vector3 newRot = new Vector3(gameObject.transform.rotation.eulerAngles.x, 0, gameObject.transform.rotation.eulerAngles.z);
-        if ((int)dir == 0)
-        {
-            newRot.y = 0;
-        }
-        else if ((int)dir == 1)
-        {
-            newRot.y = 90;
-        }
-        else if ((int)dir == 2)
-        {
-            newRot.y = 180;
-        }
-        else if ((int)dir == 3)
+		Vector3 newRot = new Vector3(gameObject.transform.rotation.eulerAngles.x, 0, gameObject.transform.rotation.eulerAngles.z);
+		if ((int)dir == 0)
 		{
-            newRot.y = 270;
-        }
+			newRot.y = 0;
+		}
+		else if ((int)dir == 1)
+		{
+			newRot.y = 90;
+		}
+		else if ((int)dir == 2)
+		{
+			newRot.y = 180;
+		}
+		else if ((int)dir == 3)
+		{
+			newRot.y = 270;
+		}
 
-        transform.eulerAngles = newRot;
-    }
+		transform.eulerAngles = newRot;
+	}
 
-    public virtual void SetDirection(Enums.Direction direct)
+	public virtual void SetDirection(Enums.Direction direct)
 	{
-        if (direct != dir)
+		if (direct != dir)
 		{
-            dir = direct; //this has to be first!
-            ChangeDirection();
-            return;
-        }
-        dir = direct;
-    }
-
-    void Animate() {
-        Vector3 positionOffset = new Vector3(progress, 0f, Mathf.Abs((float)Math.Sin(2 * Math.PI * progress)));
-        transform.localPosition = originalPosition + positionOffset;
-    }
-
-    public void Init(float offset) {
-        if (progress != -1f) return;
-
-        if (offset > 0f && offset < 1f) {
-            progress = offset;
-            reverse = true;
-        }
-    }
+			dir = direct; //this has to be first!
+			ChangeDirection();
+			return;
+		}
+		dir = direct;
+	}
 }
