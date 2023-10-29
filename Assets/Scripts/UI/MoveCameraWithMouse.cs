5ba365d5e35e659f068773757a2e643a25879c6f
Kimel-PK <57668948+Kimel-PK@users.noreply.github.com>
2023-10-29 14:09:21 +0100

    Update MoveCameraWithMouse.cs

--- Assets/Scripts/UI/MoveCameraWithMouse.cs
+++ Assets/Scripts/UI/MoveCameraWithMouse.cs
@@ -16,6 +16,7 @@ public class MoveCameraWithMouse : MonoBehaviour
     public void Move(InputAction.CallbackContext context)
     {
         Vector2 vector2 = (context.ReadValue<Vector2>() - new Vector2(1920f / 2f, 1080f / 2f)) / 1000f;
-        cameraTransform.position = initialPosition + new Vector3(vector2.x, vector2.y, 0);
+        if (cameraTransform)
+            cameraTransform.position = initialPosition + new Vector3(vector2.x, vector2.y, 0);
     }
 }
